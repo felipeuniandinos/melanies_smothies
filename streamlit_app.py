4445b5e87a930a3f544b594b975a7a26e1e53f2a
felipeuniandinos <42893684+felipeuniandinos@users.noreply.github.com>
2026-01-15 18:26:04 -0500

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 #from snowflake.snowpark.context import get_active_session  #esto es para sis no para snis
 from snowflake.snowpark.functions import col
 
@@ -50,3 +49,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
